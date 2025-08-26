f8b902678157210cfe9acd98ada27c93f85dbbfd
devHyeon99 <jinung5@gmail.com>
2025-08-26 23:27:29 +0900

    style: section 내의 배경 이미지 헤더 크기 계산해서 높이 수정

--- src/components/Section/IntroduceSection.tsx
+++ src/components/Section/IntroduceSection.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const IntroduceSection = () => {
   return (
     <section
-      className='relative flex justify-center items-center min-h-screen bg-cover bg-center'
+      className='relative flex justify-center items-center min-h-[calc(100vh-5rem)] md:min-h-[calc(100vh-6rem)] bg-cover bg-center'
       style={{ backgroundImage: `url('/images/bg.jpg')` }}
     >
       <div className='absolute inset-0 bg-black opacity-75'></div>
@@ -13,10 +13,10 @@ const IntroduceSection = () => {
       <div className='container relative grid items-center gap-8 md:grid-cols-2 z-10 text-white px-4 sm:px-6 lg:px-8'>
         <div className='flex flex-col items-center space-y-4 md:items-start'>
           <h1
-            className='text-center text-4xl font-bold leading-tight tracking-tighter md:text-left lg:text-5xl xl:text-6xl'
+            className='text-center text-3xl font-bold leading-tight tracking-tighter md:text-left lg:text-5xl xl:text-6xl'
             style={{ textShadow: '2px 2px 8px rgba(0, 0, 0, 0.7)' }}
           >
-            최고의 방역 솔루션,
+            최고의 방역 솔루션
             <br />
             <span className='text-blue-400'>스타방역공사</span>가 함께합니다.
           </h1>
@@ -29,7 +29,7 @@ const IntroduceSection = () => {
             주로 농장관리 및 소독 서비스를 제공합니다.
             <br />
             <br />
-            <span className='font-bold text-3xl text-red-400'>
+            <span className='font-bold text-lg md:text-2xl text-red-400'>
               문의 전화 : 1544-2604
             </span>
           </p>
